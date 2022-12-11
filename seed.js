6b0b799ad3e7ab240c93cb51e066094d9957b9ab
Golo <golo@GrayBrick007.fritz.box>
2022-12-11 11:19:09 +0100

    initial commit

---
+++ seed.js
@@ -0,0 +1,74 @@
+const submissions = [
+    {
+        id: 1,
+        title: 'Spaghetti Bolognese',
+        desc: 'Ein Nudelgericht mit Hackfleischsoße',
+        votes: 16,
+        country: 'Italien',
+        img: 'https://via.placeholder.com/64'
+    },
+    {
+        id: 2,
+        title: 'Peking Duck',
+        desc: 'A classic Chinese dish featuring roast duck with crispy skin and sweet hoisin sauce',
+        votes: 29,
+        country: 'China',
+        img: 'https://via.placeholder.com/64'
+    },    
+    {
+        id: 3,
+        title: 'Sushi',
+        desc: 'A Japanese dish made with vinegared rice and various toppings, such as raw fish, vegetables, and egg',
+        votes: 19,
+        country: 'Japan',
+        img: 'https://via.placeholder.com/64'
+    },    
+    {
+        id: 4,
+        title: 'Biryani',
+        desc: 'A fragrant rice dish made with spices, meat, and vegetables, popular in South Asia and the Middle East',
+        votes: 13,
+        country: 'India',
+        img: 'https://via.placeholder.com/64'
+    },    
+    {
+        id: 5,
+        title: 'Falafel',
+        desc: 'A deep-fried ball or patty made from ground chickpeas, fava beans, or a mixture of the two',
+        votes: 8,
+        country: 'Egypt',
+        img: 'https://via.placeholder.com/64'
+    },
+    {
+        id: 6,
+        title: 'Pad Thai',
+        desc: 'A stir-fried rice noodle dish commonly served as a street food and at most restaurants in Thailand',
+        votes: 32,
+        country: 'Thailand',
+        img: 'https://via.placeholder.com/64'
+    },    
+    {
+        id: 7,
+        title: 'Taco',
+        desc: 'A traditional Mexican dish consisting of a corn or wheat tortilla folded or rolled around a filling',
+        votes: 15,
+        country: 'Mexico',
+        img: 'https://via.placeholder.com/64'
+    },    
+    {
+        id: 8,
+        title: 'Pho',
+        desc: 'A Vietnamese soup made with broth, rice noodles, herbs, and meat, typically beef or chicken',
+        votes: 21,
+        country: 'Vietnam',
+        img: 'https://via.placeholder.com/64'
+    },    
+    {
+        id: 9,
+        title: 'Hamburger',
+        desc: 'A sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun',
+        votes: 18,
+        country: 'United States',
+        img: 'https://via.placeholder.com/64'
+    }    
+];
